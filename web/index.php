66336730646a4cd2cdbf36c7d024cef4c595cd18
Lucie TRUONG <Lucie.truong@etu.umontpellier.fr>
2017-05-19 10:35:19 +0200

    version 1.0.1

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
 require('../vendor/autoload.php'); 
-    require("Modele/connexion.php");
+    //require("Modele/connexion.php");
      require("Vue/commun.html");
 
 
 
    
-    if(!isset($_COOKIE["userID"])){ // L'utilisateur n'est pas connecté
+   /* if(!isset($_COOKIE["userID"])){ // L'utilisateur n'est pas connecté
         include("Controleur/connexion.php"); 
-        include("Controleur/inscription.php"); 
+        include("Controleur/inscription.php"); */
 
         //include("Controleur/inscription.php"); // On utilise un controleur secondaire pour éviter d'avoir un fichier index.php trop gros
     
